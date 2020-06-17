103fac34b5e9158a9e2e983c367a7ae1ebf3bbfb
Vitalii-Plechak <vitalii.plechak@gmail.com>
2020-06-17 22:34:30 +0300

    add home page with all section and footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,16 +1,36 @@
 $(function(){
 
-  $(".rate-star").rateYo({
-    rating: 4.5,
+  $('.rate-star').rateYo({
+    rating: 4.6,
     readOnly: true,
-    starWidth: "12px",
+    starWidth: "14px",
   });
-  
+
   $('.list__slider-inner').slick({
-    dots: false,
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
+    dots: false,
+    speed: 700,
+    nextArrow: '<button type="button" class="slick-btn slick-next lnr-chevron-right"></button>',
+    prevArrow: '<button type="button" class="slick-btn slick-prev lnr-chevron-left"></button>',
   });
 
+  $('.feed__slider-inner').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    dots: false,
+    speed: 700,
+    // autoplay: true,
+    // autoplaySpeed: 5000,
+    nextArrow: '<button type="button" class="slick-btn slick-next lnr-chevron-right"></button>',
+    prevArrow: '<button type="button" class="slick-btn slick-prev lnr-chevron-left"></button>',
+  });
+
+
+
+
+
+  var mixer = mixitup('.releaser__inner-box');
 });
